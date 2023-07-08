aa0a3b312d1c02669638851f7ec9aa7d6accc942
Bia D'Emery <biademery@gmail.com>
2023-07-08 15:31:33 -0300

    add dinamic li

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 const { VITE_WEATHER_API_KEY } = import.meta.env;
 
 export const App = () => {
-  return <>
-        </>;
+  const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+  return (
+    <>
+      <ul>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
