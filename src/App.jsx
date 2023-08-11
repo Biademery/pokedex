c278d95d0cf9c3331f950f14337994ee68a7c6c0
Biademery <biademery@gmail.com>
2023-08-11 15:24:32 -0300

    remove other components and put it all together

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { Header } from "./components/Header";
 import { PokemonCard } from "./components/PokemonCard";
-import { Footer } from "./components/Footer";
 import "./assets/css/index.css";
 
 export const App = () => {
   return (
     <>
-      <Header />
       <PokemonCard />
-      <Footer />
     </>
   );
 };
